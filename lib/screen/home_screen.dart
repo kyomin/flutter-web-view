f22f2a53c83b3bbcb1b65a3d2a4a7c4b8da17fb6
jake.kyo <jake.kyo@kakaocorp.com>
2024-04-15 02:07:03 +0900

    WebView 초기화하기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  // Constructor
+  HomeScreen({super.key});
+
+  // Variables
+  WebViewController controller = WebViewController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.orange,
-        title: Text('Web View'),
+        title: Text(
+          'Web View',
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
+      ),
+      body: WebViewWidget(
+        controller: controller,
       ),
     );
   }
